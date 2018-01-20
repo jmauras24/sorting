1539f07f00b024acc6eddff396cdb920ce5fc740
Chaehoon Lim <sizplay@gmail.com>
2018-01-19 21:14:48 -0500

    completed without spyOn method

--- bubblesort.js
+++ bubblesort.js
@@ -3,23 +3,30 @@ function swap(array, firstNumIdx, secondNumIdx) {
   array[firstNumIdx] = array[secondNumIdx];
   array[secondNumIdx] = temp;
 }
+
 function bubbleSort(array) {
 
   var numComparison = 0;
   var countSwap = 0;
 
-  for(var i = 0; i < array.length; i++) {
-
-    for(var j = 1; j < array.length; j++) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 1; j < array.length; j++) {
       numComparison++;
-      if(array[j - 1] > array[j]) {
+      if (array[j - 1] > array[j]) {
         countSwap++;
         swap(array, j - 1, j);
       }
     }
-
   }
 
-  console.log('comparisons:', numComparison, 'swap:', countSwap, 'result:',array);
+  console.log('comparisons:', numComparison, 'swap:', countSwap, 'result:', array);
   return array;
+}
+
+function bubbleSort(arr, index = arr.length - 1) {
+  if (!index) return arr;
+  for (var i = 0; i < index; i++) {
+    if (arr[i] > arr[i + 1]) swap(arr, i, i + 1);
+  }
+  return bubbleSort(arr, index - 1);
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,61 +1,25 @@
 describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
-  });
-  var testArr = [3,6,4];
+  var testArr = [6, 3],
+   testArr1 = [6, 3, 2],
+   testArr2 = [6, 3, 4, 7, 1, 2],
+   testArr3 = ['v', 'd', 'r', 'g', 's', 'q', 'c', 'a'];
 
   // beforeEach(function() {
+  //   spyOn(testArr, 'swap');
   // });
-  it('bubblesort returns sorted array', function () {
-    expect(bubbleSort(testArr)).toBe([3,4,6]);
+  it('handles an empty array', function(){
+    expect(bubbleSort([]) ).toEqual( [] );
   });
-
-
-  var arr = [3,6,4,1,8,7,2,11]
-  var sortArr = bubbleSort(arr);
-  spyOn(sortArr, "swap");
-  sortArr();
-  expect(sortArr.swap).toHaveBeenCalled();
-
-  it("should use the `swap` method", function(){
+  it('return two numbers of an array', function () {
+    expect(bubbleSort(testArr)).toEqual([3, 6]);
   });
-
-
+  it('return three numbers of an array', function () {
+    expect(bubbleSort(testArr1)).toEqual([2, 3, 6]);
+  });
+  it('return six numbers of an array', function () {
+    expect(bubbleSort(testArr2)).toEqual([1, 2, 3, 4, 6, 7]);
+  });
+  it('return multiple characters of an array', function () {
+    expect(bubbleSort(testArr3)).toEqual(["a", "c", "d", "g", "q", "r", "s", "v"]);
+  });
 });
-// describe('Bubble Sort', function(){
-//   it('counts number of swaps', function(){
-//     expect(swapCounter).toEqual(100);
-//   });
-// });
-
-// describe('palindrome', function() {
-
-//   beforeEach(function(){
-
-//   });
-
-
-
-//   it('palindrome is a function', function() {
-//     expect(typeof palindrome).toEqual('function');
-//   });
-
-//   it('palindrome returns a boolean value', function() {
-//     expect(typeof palindrome('codepen')).toEqual('boolean');
-//   });
-//   it('returns true if the argument is a palindrome', function() {
-//     expect(palindrome('racecar')).toEqual(true);
-//     expect(palindrome('madam')).toEqual(true);
-//   });
-//   it('returns false if the argument is not a palindrome', function() {
-//     expect(palindrome('Mark Davis')).toEqual(false);
-//     expect(palindrome('space jam')).toEqual(false);
-//   });
-//   it('does not use Array.prototype.reverse', function(a) {
-//     spyOn(Array.prototype, 'reverse').and.callThrough();
-//     palindrome('Hello World');
-//     expect(Array.prototype.reverse.calls.any()).toEqual(false);
-//   });
-// });
-
-
